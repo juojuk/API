c65c43b44aebecaf3f5fb0515e9f852728360f10
juojuk <juozas.juknevicius@codeacademylt.onmicrosoft.lt>
2022-12-24 02:03:04 +0200

    pridetas Measure BorrowingFeeRatio property

---
+++ API_mokymai/Migrations/20221223223927_AddMeasurePropBorrowingFeeRatio.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APImokymai.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMeasurePropBorrowingFeeRatio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "BorrowingFeeRatio",
+                table: "Measures",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BorrowingFeeRatio",
+                table: "Measures");
+        }
+    }
+}
