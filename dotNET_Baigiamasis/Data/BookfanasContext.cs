498586816719ca62eb08b6e0302b01e6201edfb3
juojuk <finanside@gmail.com>
2023-01-06 22:54:14 +0200

    update Models

---
+++ dotNET_Baigiamasis/Data/BookfanasContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dotNET_Baigiamasis.Data
+{
+    public class BookfanasContext : DbContext
+    {
+        public BookfanasContext(DbContextOptions<BookfanasContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>()
+                .HasData(
+                RoleSet.Roles
+                );
+
+            modelBuilder.Entity<Person>()
+                .HasIndex(i => i.Email)
+                .IsUnique();
+            modelBuilder.Entity<Person>()
+                .HasOne(r => r.Role)
+                .WithMany(p => p.Persons)
+                .HasForeignKey(k => k.RoleId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
---
+++ dotNET_Baigiamasis/Data/RoleSet.cs
@@ -0,0 +1,15 @@
+﻿
+namespace dotNET_Baigiamasis.Data
+{
+    public static class RoleSet
+    {
+        private static List<Role> roleList = new List<Role>()
+        {
+            new Role(1, "admin"),
+            new Role(2, "editor"),
+            new Role(3, "viewer")
+        };
+        public static List<Role> Roles { get { return roleList; } set { roleList = value; } }
+
+    }
+}
