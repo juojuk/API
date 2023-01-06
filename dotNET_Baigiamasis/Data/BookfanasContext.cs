bfb8a02630900af1c6981d8833b41a59691fbcf2
juojuk <finanside@gmail.com>
2023-01-06 22:28:44 +0200

    update DAL namespaces

---
+++ dotNET_Baigiamasis/Data/BookfanasContext.cs
@@ -0,0 +1,17 @@
+﻿using API_mokymai.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotNET_Baigiamasis.Data
+{
+    public class BookfanasContext: DbContext
+    {
+        public BookfanasContext(DbContextOptions<BookfanasContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+    }
+}
