7b30fbcd993b358d7a5b5775ff0d64e5d3fcdcd3
juojuk <finanside@gmail.com>
2023-01-08 02:18:38 +0200

    Add IRepository

---
+++ dotNET_Baigiamasis/Repository/IRepository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace dotNET_Baigiamasis.Repository.IRepository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<List<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>>? filter = null);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>>? filter, bool tracked = true);
+        Task<bool> ExistAsync(Expression<Func<TEntity, bool>> filter);
+
+        Task CreateAsync(TEntity entity);
+        Task RemoveAsync(TEntity entity);
+        Task SaveAsync();
+    }
+}
