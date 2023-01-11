c77118e08b484c0e4ee698d7e1e8c83dd14f9aa6
juojuk <finanside@gmail.com>
2023-01-11 21:27:39 +0200

    added HttpPatch

--- dotNET_Baigiamasis/Controllers/UserController.cs
+++ dotNET_Baigiamasis/Controllers/UserController.cs
@@ -1,6 +1,10 @@
-﻿using dotNET_Baigiamasis.Models.Dto;
+﻿using dotNET_Baigiamasis.Data;
+using dotNET_Baigiamasis.Models.Dto;
 using dotNET_Baigiamasis.Repository.IRepository;
+using dotNET_Baigiamasis.Services.IServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
@@ -11,20 +15,31 @@ namespace dotNET_Baigiamasis.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserRepo _userRepo;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IModelAdapter _modelAdapter;
+        private readonly IPasswordService _passwordService;
 
-        public UserController(IUserRepo userRepo)
+        public UserController(IUserRepo userRepo, IHttpContextAccessor httpContextAccessor, IModelAdapter modelAdapter, IPasswordService passwordService)
         {
             _userRepo = userRepo;
+            _httpContextAccessor = httpContextAccessor;
+            _modelAdapter = modelAdapter;
+            _passwordService = passwordService;
         }
 
         [HttpPost("login")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LoginRequest))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces(MediaTypeNames.Application.Json)]
+
         public async Task<IActionResult> LoginAsync([FromBody] LoginRequest model)
         {
             var loginResponse = await _userRepo.LoginAsync(model);
 
-            if (loginResponse.Person == null || string.IsNullOrEmpty(loginResponse.Token))
+            if (loginResponse.UserId == null || string.IsNullOrEmpty(loginResponse.Token))
             {
-                return BadRequest(new { message = "Username or password is incorrect" });
+                return BadRequest(new { message = "Invalid request" });
             }
 
             return Ok(loginResponse);
@@ -32,7 +47,6 @@ namespace dotNET_Baigiamasis.Controllers
         }
 
         [HttpPost("register")]
-        //[Authorize(Roles = "admin")]
         [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(RegistrationRequest))]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
@@ -44,7 +58,7 @@ namespace dotNET_Baigiamasis.Controllers
 
             if (!isUserNameUnique)
             {
-                return BadRequest(new { message = "Username already exists" });
+                return BadRequest(new { message = "User already exists" });
             }
 
             var user = await _userRepo.RegisterAsync(model);
@@ -56,5 +70,34 @@ namespace dotNET_Baigiamasis.Controllers
 
             return Ok();
         }
+
+        [HttpPatch("update")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status204NoContent, Type = typeof(RegistrationUpdate))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [Produces(MediaTypeNames.Application.Json)]
+
+        public async Task<IActionResult> RegistrationResetAsync(JsonPatchDocument<RegistrationUpdate> request)
+        {
+            var currentUserId = int.Parse(_httpContextAccessor.HttpContext.User.Identity.Name);
+            var user = await _userRepo.GetAsync(p => p.Id == currentUserId);
+            var registration = _modelAdapter.BindPersonWithRegistrationUpdate(user);
+
+            request.ApplyTo(registration, ModelState);
+
+            user = _modelAdapter.BindPersonWithRegistrationUpdate(user, registration);
+
+            await _userRepo.UpdateAsync(user);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return NoContent();
+        }
+
+
     }
 }
